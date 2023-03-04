22dd219f3da45d97ffb35c2e4a53c58209facc92
theabhaytyagi <126980810+theabhaytyagi@users.noreply.github.com>
2023-03-05 00:08:16 +0530

    Add files via upload

---
+++ 1stProgram.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    
+    int a,b,c;
+
+    printf("value of a = ");
+    scanf("%d",&a);
+
+    printf("value of b = ");
+    scanf("%d",&b);
+
+    c = a*a + b*b + (2*a*b);
+
+    printf("squar of a and b is = %d",c);
+
+    getch();
+
+
+    
+
+
+
+
+
+
+
+}
